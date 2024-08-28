8a3ea93479fdaf36e88387734ca9911c7de35938
istiakAHMEDsaad <istik2020@gmail.com>
2024-08-28 16:41:56 +0600

    trying practice js

---
+++ Module-17-JavaScript/Practice/1.js
@@ -0,0 +1,23 @@
+// Task 1
+let myName = 'Istiak Ahmed Saad';
+console.log(myName.length);
+// Task 2
+let firstName = 'Istiak';
+console.log(firstName[0]);
+// Task3
+console.log(firstName[firstName.length-1]);
+// Task4
+console.log('programming hero' + 10);
+// Task5
+
+let nullSpace = ' ';
+console.log(nullSpace.length)
+
+
+// Supersimple Dev Javascript Course!
+let socks = 10.90;
+let basketBall = 20.95;
+let shipping = 4.99;
+let extraTax = 4.77;
+let totalItems = ((socks*2) + (basketBall));
+console.log('Items cost: ' + totalItems);
