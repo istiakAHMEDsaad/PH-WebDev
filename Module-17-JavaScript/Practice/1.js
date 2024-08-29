2827cf73924c23de627886bb07e2e33e2bb20299
istiakAHMEDsaad <istik2020@gmail.com>
2024-08-29 19:56:09 +0600

    js practice

--- Module-17-JavaScript/Practice/1.js
+++ Module-17-JavaScript/Practice/1.js
@@ -28,3 +28,36 @@ let shippingTax = (shippingCost * 0.1);
 let totalCost = (shippingCost + shippingTax);
 console.log('tax(10%):', shippingTax.toFixed(2));
 console.log('Total[shipping + tax]:', totalCost.toFixed(2));
+console.log('\n');
+
+// Task 2a
+console.log('---------------Task 2A---------------');
+let soupPrice = 10;
+let burgerPrice = 8;
+let icePrice = 5;
+let totalCost1 = (soupPrice + (burgerPrice * 3) + icePrice);
+console.log(`Toatl Cost: ${totalCost1}$\n`);
+
+// Task 2b
+console.log('---------------Task 2B---------------');
+console.log(`Three person order total cost: ${(totalCost1*3).toFixed(2)}$`);
+console.log(`Per person avg cost: ${((totalCost1*3) / 3).toFixed(2)}$\n`);
+
+// Task 2c
+console.log('---------------Task 2C---------------');
+let tosterPrice = 18.50;
+let shirtPrice = 7.50;
+let totalCost2 = tosterPrice + (2 * shirtPrice);
+console.log(`Total price: ${totalCost2}$\n`);
+
+// Task 2d
+console.log('---------------Task 2D---------------');
+let tax = totalCost2 * 0.1;
+console.log(`10% of tax: ${tax.toFixed(2)}$`);
+console.log(`Total price with 10% of tax: ${tax + totalCost2}$\n`);
+
+// Task 2e
+console.log('---------------Task 2E---------------');
+let tax2 = totalCost2 * 0.2;
+console.log(`20% of tax: ${tax2}$`);
+console.log(`Final price: ${(totalCost2 + tax2).toFixed(2)}$\n`);
