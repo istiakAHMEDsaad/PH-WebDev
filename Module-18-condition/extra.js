8ed3b19b97ce570a3b69bd5541719e1aa50360a0
istiakAHMEDsaad <istik2020@gmail.com>
2024-09-02 17:49:28 +0600

    loops

--- Module-18-condition/extra.js
+++ Module-18-condition/extra.js
@@ -26,52 +26,46 @@ switch (quarter) {
     console.log("Quarter 1, 2, 3, 4 only");
 }
 
-
 // Question 4
-let stringFive = 'Apple';
-((stringFive[0] == 'a') || (stringFive[0] == 'A') && (stringFive.length >= 5))
-    ? console.log('Golden String')
-    : console.log('Bad String');
+let stringFive = "Apple";
+stringFive[0] == "a" || (stringFive[0] == "A" && stringFive.length >= 5)
+  ? console.log("Golden String")
+  : console.log("Bad String");
 
 // Question 5
 let numbers = [1, 2, 3];
-if(numbers[0] < numbers[1] && numbers[0] > numbers[2]){
-    console.log('Largest number:', numbers[1]);
-}
-else if(numbers[0] > numbers[1] && numbers[1] > numbers[2]){
-    console.log('Largest number:', numbers[0]);
-}
-else if(numbers[1] < numbers[2] && numbers[1] > numbers[0]){
-    console.log('Largest number:', numbers[2]);
-}
-else if(numbers[1] > numbers[2] && numbers[2] > numbers[0]){
-    console.log('Largest number:',numbers[1]);
-}
-else if(numbers[0] > numbers[2] && numbers[2] < numbers[1]){
-    console.log('Largest number:', numbers[0]);
-}
-else if(numbers[0] < numbers[2] && numbers[0] > numbers[1]){
-    console.log('Largest number:', numbers[2]);
-}
-else{
-    console.log('***');
+if (numbers[0] < numbers[1] && numbers[0] > numbers[2]) {
+  console.log("Largest number:", numbers[1]);
+} else if (numbers[0] > numbers[1] && numbers[1] > numbers[2]) {
+  console.log("Largest number:", numbers[0]);
+} else if (numbers[1] < numbers[2] && numbers[1] > numbers[0]) {
+  console.log("Largest number:", numbers[2]);
+} else if (numbers[1] > numbers[2] && numbers[2] > numbers[0]) {
+  console.log("Largest number:", numbers[1]);
+} else if (numbers[0] > numbers[2] && numbers[2] < numbers[1]) {
+  console.log("Largest number:", numbers[0]);
+} else if (numbers[0] < numbers[2] && numbers[0] > numbers[1]) {
+  console.log("Largest number:", numbers[2]);
+} else {
+  console.log("***");
 }
 
 // Question 6(bonus)
-let num1 = '32';
-let num2 = '47852';
-if(num1[num1.length-1] == num2[num2.length-1]){
-    console.log('Last two digit same!');
-}
-else{
-    console.log('Last two digit is not same');
+/* Qs6 (Bonus). Write a program to check if 2 numbers have the same last digit.
+Eg: 32 and 47852 have the same last digit i.e. 2
+*/
+let num1 = "32";
+let num2 = "47852";
+if (num1[num1.length - 1] == num2[num2.length - 1]) {
+  console.log("Last two digit same!");
+} else {
+  console.log("Last two digit is not same");
 }
 
 let num3 = 32 % 10;
 let num4 = 47852 % 10;
-if(num3 == num4){
-    console.log('same');
-}
-else{
-    console.log('diff');
+if (num3 == num4) {
+  console.log("same");
+} else {
+  console.log("diff");
 }
---
+++ Module-19-loop/1.js
@@ -0,0 +1,26 @@
+// For
+for(let i=0; i<5; i++){
+    console.log(`${i+1}. I love you`);
+}
+console.log(`---------------------------- \n`);
+for(let i=0; i<5; i++){
+    console.log(`${i+1}. Tajrin hate me so much :'-(`);
+}
+console.log(`---------------------------- \n`);
+// While
+let nume = 0;
+while (nume <= 10) {
+    console.log(`${nume+1}. Unnoyon bal amar`);
+    nume++;
+}
+console.log(`---------------------------- \n`);
+let num = 0;
+while(num <= 9){
+    console.log(`Number ${num+1}`);
+    num++;
+}
+console.log(`---------------------------- \n`);
+let arrOne = ['Shoni', 'Robi', 'Shom'];
+for(const din of arrOne){
+    console.log(din);
+}
