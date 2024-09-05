b89f16fd9e9036ec8abf7356cbb75eb98770306a
istiakAHMEDsaad <istik2020@gmail.com>
2024-09-05 23:23:48 +0600

    function

---
+++ Module-19-loop/array_loops.js
@@ -0,0 +1,11 @@
+let heros = [["ironman","spiderman","thor"], ["superman", "wonder woman", "flash"]];
+for(let i=0; i<heros.length; i++){
+    console.log(`List ${i+1}`);
+    for(let j=0;j<heros[i].length;j++){
+        console.log(`Heros: ${heros[i][j]}`);
+    }
+}
+
+for(char of "saad"){
+    console.log(char);
+}
--- Module-19-loop/break.js
+++ Module-19-loop/break.js
@@ -15,16 +15,16 @@ for (i; i <= 200; i++) {
 console.log("\n--------------------Task 2--------------------\n");
 let i2 = 1;
 let sum = 0;
-while (true) {
-  sum += i2;
-  if (sum >= 100) {
-    break;
-  }
-  i2++;
+while(i2<=150){
+    if(i2>50){
+        break;
+    }
+    sum += i2;
+    console.log(i2);
+    i2++;
 }
-console.log(`sum of 100: ${sum}`);
+console.log(`Sum: ${sum}`); 
 
-  
 
 // Task 3
 /* console.log("\n--------------------Task 3--------------------\n");
