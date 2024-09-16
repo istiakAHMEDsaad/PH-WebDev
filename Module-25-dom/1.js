88a1e645435fab4b4a145fe200ba6160f479bd1d
istiakAHMEDsaad <istik2020@gmail.com>
2024-09-16 23:11:38 +0600

    dom

---
+++ Module-25-dom/1.js
@@ -0,0 +1,36 @@
+// console.log(document)
+const tempName = {
+  name: "Saad",
+  id: "22234103008",
+  study: function (timeSt) {
+    console.log(`I have been stuyding last ${timeSt}hours`);
+  },
+};
+
+// Multiple class selector
+const colorMe = document.getElementsByClassName("color-me");
+for (const colors of colorMe) {
+  console.log(colors.innerText);
+}
+
+// Tag array but not array
+const liDekha = document.getElementsByTagName("li");
+for (const lis of liDekha) {
+  console.log(lis.innerText);
+}
+
+// Heading 2 edit
+const headerDekha = document.getElementsByTagName("h2");
+for (const heads of headerDekha) {
+  console.log(heads.innerText);
+}
+
+// ID edit
+const changeMe = document.getElementById("change-me");
+changeMe.innerText = "Noo! JS bastard change my text!";
+
+// query selector
+const amraKori = document.querySelectorAll(".amra-kori span");
+for (const amras of amraKori) {
+  console.log(amras);
+}
---
+++ Module-25-dom/2.js
@@ -0,0 +1,9 @@
+// Change Style with js
+// document.getElementsByClassName('heading1');
+document.getElementById("heading1").style.backgroundColor="aqua";
+document.getElementById("heading1").style.color="tomato";
+document.getElementById("heading1").style.textAlign="center";
+
+const changeMe = document.getElementById("cng-maro");
+// changeMe.getAttribute("id");
+// changeMe.getAttribute("class");
