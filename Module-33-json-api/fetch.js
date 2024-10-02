7b4427d23c5fb0ea43cdcc0cf37da0b4e9be2d6e
istiakAHMEDsaad <istik2020@gmail.com>
2024-10-03 00:45:27 +0600

    more more js

--- Module-33-json-api/fetch.js
+++ Module-33-json-api/fetch.js
@@ -8,12 +8,37 @@ function dummyData() {
 function dummyUsers() {
   fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => showUserData(json));
+}
+function showUserData(json) {
+  for (const user of json) {
+    console.log(user.username);
+  }
 }
 
-function pokeData() {
-  fetch('https://pokeapi.co/api/v2/pokemon/mew')
+/* function pokeData() {
+  fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
     .then((response) => response.json())
-    .then((json) => console.log(json))
+    .then((json) => showPokemonData(json))
     .catch((error) => console.log(error));
-}
+} */
+
+document
+  .getElementById('pokemon-btn')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const inputValue = document.getElementById('pokemonInput').value;
+
+    fetch('https://pokeapi.co/api/v2/pokemon/'+inputValue)
+      .then((response) => response.json())
+      .then((json) => showPokemonData(json))
+      .catch((error) => console.log(error));
+
+      const showName = function showPokemonData(json){
+        console.log(json.name);
+      }
+    const createPara = document.createElement('p');
+    createPara.innerText = `Pokemon name ${showName(json)}`;
+    document.getElementById('showPoke').appendChild(createPara);
+  });
