71ef954675adc7ea75a37b5eec9d4a6fe2a75aa2
istiakAHMEDsaad <istik2020@gmail.com>
2024-10-04 11:33:06 +0600

    set interval

---
+++ Module-34-more-js/js/setinterval.js
@@ -0,0 +1,16 @@
+console.log('first');
+console.log('second');
+setTimeout(() => {
+  console.log(3);
+}, 3000);
+console.log('fourth');
+console.log('fifth');
+
+let num = 0;
+const intervalId = setInterval(() => {
+    num++;
+    console.log(`countdown: ${num}`);
+    if(num>=60){
+        clearInterval(intervalId)
+    }
+}, 1000)
