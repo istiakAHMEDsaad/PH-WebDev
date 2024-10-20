d3b4fe08c31778b442a41293ef838deb01ba337c
istiakAHMEDsaad <istik2020@gmail.com>
2024-10-20 18:28:50 +0600

    prop handle

--- Module-40-rest/src/components/Countries.jsx
+++ Module-40-rest/src/components/Countries.jsx
@@ -3,24 +3,40 @@
 import { useEffect, useState } from 'react';
 import Country from './Country';
 
+//TODO1: Main Function
 const Countries = () => {
   const [countries, setCountires] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([])
 
+  //TODO3: Fetch data by useEffect hook
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
       .then((res) => res.json())
       .then((data) => setCountires(data))
       .catch((error) => console.error(error));
   }, []);
+
+  //TODO4: handle visited countries
+  const handleVisitedCountries = (country) => {
+    console.log('Add this to your visited country');
+    console.log(country);
+  }
+
+  //TODO2: Return
   return (
     <div>
       <h3 className='text-3xl text-sky-500 text-center'>
         Total Countries: <span className='italic'>{countries.length}</span>
       </h3>
 
+      <div>
+        <h4>Visited Country</h4>
+        <ul></ul>
+      </div>
+
       <div className='grid grid-cols-1 md:gap-x-4 lg:grid-cols-3 md:grid-cols-2'>
         {countries.map((country) => (
-          <Country key={country.cca3} country={country}></Country>
+          <Country key={country.cca3} country={country} handleVisitedCountries={handleVisitedCountries}></Country>
         ))}
       </div>
     </div>
--- Module-40-rest/src/components/Country.jsx
+++ Module-40-rest/src/components/Country.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries }) => {
   const { name, flags, area, population, capital, cca3 } = country;
 
   const [visited, setVisited] = useState(false);
@@ -10,10 +10,10 @@ const Country = ({ country }) => {
   const handleVisited = () => {
     setVisited(!visited);
   };
-
+  console.log(handleVisitedCountries);
   return (
     <div className='py-3'>
-      <div className='lg:h-[425px] flex flex-col items-center justify-center border border-teal-500 rounded-md p-4 space-y-2'>
+      <div className={`lg:h-[425px] flex flex-col items-center justify-center border border-teal-500 rounded-md p-4 space-y-2 ${visited ? 'bg-slate-500 text-black' : ''}`}>
         <h2 className='text-xl text-yellow-500'>
           Name: <span>{name?.common}</span>
         </h2>
@@ -45,10 +45,18 @@ const Country = ({ country }) => {
 
         <button
           onClick={handleVisited}
-          className='border border-stone-500 px-4 py-2 rounded-md hover:bg-gray-600 transition-all'
+          className={`border border-stone-500 px-4 py-2 rounded-md hover:bg-gray-600 transition-all ${visited ? 'bg-red-500' : ''}`}
+        >
+          {visited ? 'Added' : 'Add to Visited'}
+        </button>
+
+        <button
+          onClick={handleVisited}
+          className={`border border-stone-500 px-4 py-2 rounded-md hover:bg-gray-600 transition-all ${visited ? 'bg-red-500' : ''}`}
         >
           {visited ? 'Visited' : 'Not visit'}
         </button>
+        
         {visited ? 'I already visited this country' : 'I want to visit'}
       </div>
     </div>
