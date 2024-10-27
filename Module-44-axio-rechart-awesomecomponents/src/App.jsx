93ebee67c6a5eeb37939f8082abdfd9b82db0a2e
istiakAHMEDsaad <istik2020@gmail.com>
2024-10-28 01:18:18 +0600

    module - 44

---
+++ Module-44-axio-rechart-awesomecomponents/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Test from './components/Test';
+
+function App() {
+  return (
+    <>
+      <div>
+        <Test></Test>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Module-44-axio-rechart-awesomecomponents/tailwind.config.js
@@ -0,0 +1,19 @@
+import daisyui from 'daisyui';
+import { plugins } from 'tailwindcss';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+}
