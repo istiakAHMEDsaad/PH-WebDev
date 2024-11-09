3d508d79e5d4b19264101acdaa9699e571550129
istiakAHMEDsaad <istik2020@gmail.com>
2024-11-09 20:27:29 +0600

    login logout

--- Module-49-auth/src/Pages/Login.jsx
+++ Module-49-auth/src/Pages/Login.jsx
@@ -1,19 +1,84 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
 import auth from '../Auth/firebase.init';
+import { useState } from 'react';
+import { toast, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Login = () => {
   const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
 
   const handleGooglSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         console.log(result);
+        setUser(result.user);
+        toast.success('Login Success!', {
+          position: 'top-center',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'colored',
+          transition: Bounce,
+        });
       })
-      .catch((error) => console.log('Error', error));
+      .catch((error) => {
+        console.log('Error', error);
+        setUser(null);
+        toast.error('Somethings wrong!', {
+          position: 'top-center',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'colored',
+          transition: Bounce,
+        });
+      });
   };
 
+  const handleGooglSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        toast.success('Log out successfully!', {
+          position: 'top-center',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'colored',
+          transition: Bounce,
+        });
+        setUser(null);
+      })
+      .catch((error) => {
+        toast.error('Somethings wrong!', {
+          position: 'top-center',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'colored',
+          transition: Bounce,
+        });
+        console.log('Error', error);
+      });
+  };
+  const { displayName, email, photoURL } = user || {};
+
   return (
-    <div>
+    <div className='flex flex-col gap-y-10'>
+      {/* ==========> Form <========== */}
       <form className='flex max-w-md flex-col gap-4'>
         <div>
           <div className='mb-2 block'>
@@ -38,19 +103,43 @@ const Login = () => {
         </div>
         <Button
           type='submit'
-          className='border py-1 hover:bg-gray-200 transition-all'
+          className='text-black border py-1 hover:bg-gray-200 transition-all'
         >
           Submit
         </Button>
       </form>
 
-      <div>
-        <button
-          onClick={handleGooglSignIn}
-          className='border py-2 px-4 rounded-lg hover:bg-rose-300 transition-all'
-        >
-          Click me
-        </button>
+      {/* ==========> Button <========== */}
+      <div className='border-2 border-dashed border-gray-500 rounded-md space-y-5'>
+        {user && (
+          <div className='flex flex-col'>
+            <div>
+              <img src={photoURL} alt='' />
+            </div>
+            <h4 className='text-2xl font-bold'>
+              Display Name: <span className='font-normal'>{displayName}</span>
+            </h4>
+            <p className='text-2xl'>
+              Email: <span className='font-normal'>{email}</span>
+            </p>
+          </div>
+        )}
+
+        {user ? (
+          <button
+            onClick={handleGooglSignOut}
+            className='border py-2 px-4 rounded-lg bg-rose-500 text-white font-medium hover:bg-rose-500/90 active:scale-95 transition-all '
+          >
+            Sign Out
+          </button>
+        ) : (
+          <button
+            onClick={handleGooglSignIn}
+            className='border py-2 px-4 rounded-lg bg-blue-500 text-white font-medium hover:bg-blue-500/90 active:scale-95 transition-all '
+          >
+            Click me
+          </button>
+        )}
       </div>
     </div>
   );
--- Module-49-auth/src/main.jsx
+++ Module-49-auth/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Routes';
+import { ToastContainer } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}></RouterProvider>
+    <ToastContainer></ToastContainer>
   </StrictMode>
 );
