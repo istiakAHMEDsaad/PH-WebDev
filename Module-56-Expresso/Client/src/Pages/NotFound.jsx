8e323407d59fc371f505e35fb19ca62b8b94ef32
istiakAHMEDsaad <istik2020@gmail.com>
2024-12-11 22:46:43 +0600

    module-58

--- Module-56-Expresso/Client/src/Pages/NotFound.jsx
+++ Module-56-Expresso/Client/src/Pages/NotFound.jsx
@@ -3,18 +3,18 @@ import { FaLongArrowAltLeft } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-    return (
-        <div className=''>
-            <Link to={'/'}>
+  return (
+    <div className=''>
+      <Link to={'/'}>
         <button className='font-rancho text-2xl flex items-center gap-1 hover:scale-105 transition-all active:scale-95'>
           <FaLongArrowAltLeft></FaLongArrowAltLeft>Back to home
         </button>
       </Link>
-            <div className='flex flex-col items-center justify-center w-full h-screen'>
-            <img src={notFound} alt="404 not found" />
-            </div>
-        </div>
-    );
+      <div className='flex flex-col items-center justify-center w-full h-screen'>
+        <img src={notFound} alt='404 not found' />
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
--- Module-56-Expresso/Client/src/Pages/SignUp.jsx
+++ Module-56-Expresso/Client/src/Pages/SignUp.jsx
@@ -1,11 +1,53 @@
-
+import { FaLongArrowAltLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
-    return (
-        <div>
-            <h2>Sign Up</h2>
+  return (
+    <div className='my-10'>
+      <Link to={'/'}>
+        <button className='font-rancho text-2xl flex items-center gap-1 hover:scale-105 transition-all active:scale-95 mb-5'>
+          <FaLongArrowAltLeft></FaLongArrowAltLeft>Back to home
+        </button>
+      </Link>
+
+      <div className='flex flex-col items-center justify-center'>
+        <div className='card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl'>
+          <form className='card-body'>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Email</span>
+              </label>
+              <input
+                type='email'
+                placeholder='email'
+                className='input input-bordered'
+                required
+              />
+            </div>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Password</span>
+              </label>
+              <input
+                type='password'
+                placeholder='password'
+                className='input input-bordered'
+                required
+              />
+              <label className='label'>
+                <a href='#' className='label-text-alt link link-hover'>
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className='form-control mt-6'>
+              <button className='btn btn-neutral bg-brown'>Login</button>
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
