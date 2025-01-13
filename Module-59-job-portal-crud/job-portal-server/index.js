24179f8163783970e2e61fcfa799cf8aeac20e8f
istiakAHMEDsaad <istik2020@gmail.com>
2025-01-13 17:39:10 +0600

    ref

--- Module-59-job-portal-crud/job-portal-server/index.js
+++ Module-59-job-portal-crud/job-portal-server/index.js
@@ -1,4 +1,5 @@
 /* ----------- Import Module ----------- */
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,15 +8,67 @@ const port = process.env.PORT || 3000;
 /* ----------- Cors ----------- */
 app.use(cors());
 app.use(express.json());
+require('dotenv').config();
+
+
+/* ---------------------------------------------- MongoDB ---------------------------------------------- */
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@my-mongodb.2rdes.mongodb.net/?retryWrites=true&w=majority&appName=My-MongoDB`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    /* --> Job Related API <-- */
+    const database = client.db("job_portal");
+    const jobsCollection = database.collection("jobs");
+
+    //get jobs data 
+    app.get('/jobs', async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get jobs id
+    app.get(`/jobs/:id`, async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    })
+
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+/* ----------------------------------------------------------------------------------------------------- */
 
 
 /* ----------- Method ----------- */
 app.get('/', (req, res) => {
-    res.send({
-        "jobServer": "Job server is running local 3000 port",
-    })
-})
+  res.send({
+    "jobServer": "Job server is running local 3000 port",
+  });
+});
 
-app.listen(port, ()=>{
-    console.log(`Server is connected successfully, port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is connected successfully, port: ${port}`);
+});
